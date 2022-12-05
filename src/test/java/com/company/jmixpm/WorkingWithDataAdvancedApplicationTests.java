7d45d2ff5b5e138996cb65f7596c6ab260642eb8
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-12-05 18:12:40 +0400

    Initial worksapce

---
+++ src/test/java/com/company/jmixpm/WorkingWithDataAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.jmixpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingWithDataAdvancedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
